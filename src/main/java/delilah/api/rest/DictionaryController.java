b0abb8168bf4163eedcc3432b734918319143d78
Alec Lévesque <alec.levesque.1@ulaval.ca>
2023-09-26 19:18:48 -0400

    Added dictionary entry creation and deletion endponts

--- src/main/java/delilah/api/rest/DictionaryController.java
+++ src/main/java/delilah/api/rest/DictionaryController.java
@@ -1,13 +1,11 @@
 package delilah.api.rest;
 
+import delilah.api.rest.dto.CreateDictionaryEntryDTO;
 import delilah.api.rest.dto.DiscordUserDTO;
-import delilah.domain.models.dictionnary.Dictionary;
+import delilah.domain.models.dictionnary.DictionaryEntry;
 import delilah.services.DictionaryService;
 import delilah.services.discord.DiscordUserVerificationService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dictionary")
@@ -21,10 +19,18 @@ public class DictionaryController {
         this.discordUserVerificationService = discordUserVerificationService;
     }
 
-    @GetMapping()
-    public Dictionary getDictionaryEntries(@RequestHeader("access_token") String accessToken) {
-        DiscordUserDTO userDTO = discordUserVerificationService.verifyUserFromAccessToken(accessToken);
+    @PostMapping("/entry")
+    public void createEntry(@RequestHeader("access_token") String accessToken, @RequestBody CreateDictionaryEntryDTO createDictionaryEntryDTO) {
+        DiscordUserDTO discordUserDto = discordUserVerificationService.verifyUserFromAccessToken(accessToken);
 
-        return dictionaryService.getDictionaryForUser(userDTO.id);
+        dictionaryService.addToUserDictionary(discordUserDto.id, createDictionaryEntryDTO.word, createDictionaryEntryDTO.definition);
+    }
+
+    @CrossOrigin
+    @DeleteMapping("/entry")
+    public void deleteWord(@RequestHeader("access_token") String accessToken, @RequestParam() String word) {
+        DiscordUserDTO discordUserDto = discordUserVerificationService.verifyUserFromAccessToken(accessToken);
+
+        dictionaryService.removeDictionaryEntryForUser(discordUserDto.id, word);
     }
 }
